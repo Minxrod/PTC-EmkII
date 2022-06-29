c8709a2f5f4525853dfb53b2bc6763ba9ce96c5f
Minxrod <minxrod@gmail.com>
2022-06-29 15:43:52 -0400

    Docs for the Drawables

--- src/SpriteArray.hpp
+++ src/SpriteArray.hpp
@@ -89,17 +89,36 @@ struct vec {
 /// @return true if sprites are touching
 bool is_hit(SpriteInfo& a, SpriteInfo& b);
 
+/// Drawable collection of sprites.
+/// 
+/// This class manages a list of drawable sprites. Sprites can be added and moved, but not deleted.
 class SpriteArray : public sf::Drawable, public sf::Transformable {
+	/// VertexArray used for rendering sprites
 	sf::VertexArray va{sf::Quads, 0};
+	/// Map of sprite ids to sprite vertex ranges, used for updating vertices after a sprite was added.
 	std::map<int, std::pair<int, int>> va_index; //id:[start_index, end_index)
 	
 public:
+	/// Constructor
 	SpriteArray();
 	
-	void add_sprite(const SpriteInfo&);
-	void update_sprite_xy(const SpriteInfo&, int, int);
+	/// Adds a sprite to this SpriteArray.
+	/// 
+	/// @param s Sprite to add
+	void add_sprite(const SpriteInfo& s);
+	/// Updates the position of a sprite in this SpriteArray.
+	/// @note Updates relative to current position.
+	/// 
+	/// @param s Sprite to update
+	/// @param xofs Offset x location
+	/// @param yofs Offset y location
+	void update_sprite_xy(const SpriteInfo& s, int xofs, int yofs);
 	
-	void draw(sf::RenderTarget&, sf::RenderStates) const override;
+	/// Draws this collection of sprites.
+	///
+	/// @param r Target to draw to (ex. sf::RenderWindow)
+	/// @param s States to draw with (includes shader, texture, etc.)
+	void draw(sf::RenderTarget& r, sf::RenderStates s) const override;
 };
 
 
--- src/TileMap.hpp
+++ src/TileMap.hpp
@@ -3,27 +3,72 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 
+/// Drawable tilemap class.
+/// 
+/// Used for a grid of renderable tiles, such as a console or BG layer. 
+/// 
+/// @note Only intended to be drawn with the palette shader.
 class TileMap : public sf::Drawable, public sf::Transformable {
+	/// Width of this tilemap, in tiles
 	int width;
+	/// Height of this tilemap, in tiles
 	int height;
+	/// VertexArray to store the polygons used by SFML for rendering.
 	sf::VertexArray va;
+	/// View used to set clipping bounds.
 	sf::View view;
 	
 public:
-	TileMap(int, int);
+	/// Constructor
+	/// 
+	/// @param w Width of map
+	/// @param h Width of map
+	TileMap(int w, int h);
 	
+	/// Default constructor (deleted)
 	TileMap() = delete;
 	
+	/// Copy constructor (default)
 	TileMap(const TileMap&) = default;
 	
+	/// Copy assignment (default)
 	TileMap& operator=(const TileMap&) = default;
 	
-	void tile(int, int, int);
+	/// Sets a tile with the given information.
+	/// 
+	/// @param x Tile x location
+	/// @param y Tile y location
+	/// @param chr Character code
+	void tile(int x, int y, int chr);
+	/// Sets a tile with the given information.
+	/// 
+	/// @param x Tile x location
+	/// @param y Tile y location
+	/// @param chr Character code
+	/// @param h true=Horizontally flip texture
+	/// @param v true=Vertically flip texture
 	void tile(int x, int y, int chr, bool h, bool v);
-	void palette(int, int, int, int = 0);
-	void clip(int, int, int, int);
+	/// Sets the palette of the tile at (x,y).
+	/// 
+	/// @param x Tile x location
+	/// @param y Tile y location
+	/// @param pal Palette number
+	/// @param bg Background color (default=0)
+	void palette(int x, int y, int pal, int bg = 0);
+	/// Sets the clipping rectangle for this TileMap.
+	/// @note Rectangle is inclusive of tiles along x=x2 or y=y2.
+	/// 
+	/// @param x1 Corner 1 x location (upper left)
+	/// @param y1 Cornet 1 y location (upper left)
+	/// @param x2 Corner 2 x location
+	/// @param y2 Cornet 2 y location
+	void clip(int x1, int y1, int x2, int y2);
 	
-	void draw(sf::RenderTarget&, sf::RenderStates) const override;
+	/// Draws this tilemap.
+	///
+	/// @param t Target to draw to (ex. sf::RenderWindow)
+	/// @param s States to draw with (includes shader, texture, etc.)
+	void draw(sf::RenderTarget& t, sf::RenderStates s) const override;
 };
 
 
